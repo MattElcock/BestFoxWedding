b73e130044b14aca4781367234897cb0818b3486
Matt Elcock <matthew.elcock82@googlemail.com>
2025-11-23 11:05:55 +0000

    Flesh out venue overview

--- src/app/venue/page.tsx
+++ src/app/venue/page.tsx
@@ -19,12 +19,50 @@ export default function venue() {
       </div>
       <h1>The Temple of Peace</h1>
       <p>
-        Founded in 1938, the Temple of Peace was built as a memorial to those
-        who died in the First World War. It's home to the Welsh Book of
-        Rememberance, containing the names of 35,000 servicemen and woman who
-        lost their lives in the conflict. Its founder, Lord David Davies of
-        Llandinam, hoped it would become a place of pilgramage to everyone in
-        search of justice, health and peace.
+        Built in 1938, the Temple of Peace is a memorial to those who died in
+        the First World War and home to the{" "}
+        <a
+          href="https://www.library.wales/discover-learn/digital-exhibitions/manuscripts/modern-period/the-welsh-national-book-of-remembrance"
+          target="_blank"
+          referrerPolicy="no-referrer"
+        >
+          Welsh Book of Rememberance
+        </a>
+        , which contains the names of over 35,000 people who died in the
+        conflict.
+      </p>
+      <p>
+        Once open, it became the headquarters of two significant Welsh
+        organisations. In the Health Wing, the{" "}
+        <a
+          href="https://www.wcia.org.uk/wnma/"
+          target="_blank"
+          referrerPolicy="no-referrer"
+        >
+          Welsh National Memorial Association
+        </a>
+        , worked to eradicate tuberculosis in Wales. In the Peace Wing, the{" "}
+        <a
+          href="https://www.wcia.org.uk/wlnu/"
+          target="_blank"
+          referrerPolicy="no-referrer"
+        >
+          Welsh League of Nations Union
+        </a>{" "}
+        supported the Welsh in campaigning for international cooperation and
+        peace.
+      </p>
+      <p>
+        Its founder,{" "}
+        <a
+          href="https://www.wcia.org.uk/wcia-news/wcia-history/david-davies-75-father-of-the-temple-of-peace/"
+          target="_blank"
+          referrerPolicy="no-referrer"
+        >
+          Lord David Davies of Llandinam
+        </a>
+        , hoped it would become a place of pilgramage to everyone in search of
+        justice, health and peace.
       </p>
       <p>
         <a
