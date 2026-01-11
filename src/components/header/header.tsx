0f0f2f2300af55ee1c605e21723f70c4047d9262
Matt Elcock <matthew.elcock82@googlemail.com>
2026-01-11 13:50:25 +0000

    Hide logout when logged out

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import "./header.css";
 import { LogOut } from "lucide-react";
+import { useUser } from "@auth0/nextjs-auth0";
 
 const Header = () => {
+  const { user } = useUser();
+
   return (
     <header>
       <Link href="/" className="site-title">
@@ -18,9 +23,11 @@ const Header = () => {
           <p>Anthony and Lloyd</p>
         </div>
       </Link>
-      <a href="/auth/logout" aria-label="Log Out">
-        <LogOut />
-      </a>
+      {user && (
+        <a href="/auth/logout" aria-label="Log Out">
+          <LogOut />
+        </a>
+      )}
     </header>
   );
 };
