c0fa9c8cd04e5591d30bbfa13931b3563ba72831
Matt Elcock <matthew.elcock82@googlemail.com>
2026-01-17 14:39:24 +0000

    color and spacing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default async function RootLayout({
       <body>
         <Header />
         <main>
-          <div className="page">{children}</div>
+          <div>{children}</div>
         </main>
         <Analytics />
       </body>
