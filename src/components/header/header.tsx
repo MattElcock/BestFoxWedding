5857e10bfd718a238e298d7a89147e3c8d02ec1b
Matt Elcock <matthew.elcock82@googlemail.com>
2026-04-11 14:06:12 +0100

    homepage font size tweaks

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,24 +1,11 @@
-"use client";
-
 import Link from "next/link";
-import Image from "next/image";
 import "./header.css";
-import { LogOut } from "lucide-react";
 
 const Header = () => {
   return (
     <header>
       <Link href="/" className="site-title">
-        <Image
-          width={35}
-          height={35}
-          src="/images/cats.svg"
-          alt="Art Deco Cats"
-        />
-        <div>
-          <p>The Wedding of</p>
-          <p>Anthony and Lloyd</p>
-        </div>
+        Anthony and Lloyd
       </Link>
     </header>
   );
