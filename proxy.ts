e75c6518855b7efa0315a4bbea07295f25bfccd1
Matt Elcock <matthew.elcock82@googlemail.com>
2026-04-12 15:06:55 +0100

    Revert "Force users to choose a language on first visit"
    This reverts commit 7ea80792b3d1bb45e200be88c9a87a33728c81dd.

--- proxy.ts
+++ proxy.ts
@@ -1,30 +1,10 @@
 import { createProxy } from "next-i18next/proxy";
 import i18nConfig from "./i18n.config";
-import { NextRequest, NextResponse } from "next/server";
 
-export const proxy = (request: NextRequest) => {
-  /*
-   * Check if user has already chosen a language.
-   * If not, redirect them to make a choice.
-   */
-  const cookies = request.cookies;
-  const next = request.nextUrl.pathname.replace("/en", "").replace("/cy", "");
-
-  const isOnSelectLanguagePage =
-    request.nextUrl.pathname === "/en/select-language";
-  const chosenLanguage = cookies.get("SHOWN_CHOOSE_LANGUAGE_PAGE");
-
-  if (!chosenLanguage && !isOnSelectLanguagePage) {
-    return NextResponse.redirect(
-      new URL(`/en/select-language?next=${next}`, request.url),
-    );
-  }
-
-  return createProxy(i18nConfig)(request);
-};
+export const proxy = createProxy(i18nConfig);
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|images|assets|favicon.ico|sw.js|site.webmanifest).*)",
+    "/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)",
   ],
 };
