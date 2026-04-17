00008758ee2102728882ee7d59afe2f3bab3979f
Matt Elcock <matthew.elcock82@googlemail.com>
2026-04-17 20:04:57 +0100

    Remove choose initial locale page

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,27 +1,7 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
-import { NextRequest, NextResponse } from "next/server";
 
-export default function proxy(request: NextRequest) {
-  const page = request.nextUrl.pathname;
-
-  if (!page.endsWith("/choose-locale")) {
-    const shownChooseLanguagePage = request.cookies.get(
-      "SHOWN_CHOOSE_LANGUAGE_PAGE",
-    );
-
-    if (!shownChooseLanguagePage) {
-      const locale =
-        request.cookies.get("NEXT_LOCALE")?.value || routing.defaultLocale;
-      const url = request.nextUrl.clone();
-
-      url.pathname = `/${locale}/choose-locale`;
-      return NextResponse.redirect(url);
-    }
-  }
-
-  return createMiddleware(routing)(request);
-}
+export default createMiddleware(routing)
 
 export const config = {
   // Match all pathnames except for
